e3456f831b091879bb41d5769e0ee6fcd13195f0
mow09 <donj31313@gmail.com>
2019-09-03 14:39:01 +0200

    reworke

--- README.md
+++ README.md
@@ -1,16 +1,38 @@
-## making PDF put of python with LaTeX
+# Making Document
 
-- [ ] add subtitle
-- [ ] math
-	- [x] MAtrix
-	- [x] Vector
-	- [ ] Graphs
-	- [ ] Questions, Exercises
-	- [ ] create real exercies
-		- [ ] save random seed
-- [ ] function for solo column text
-- [ ] QR
-	- [ ] contains random seed for corection
-	- [ ] analysable
+Is there to speed up good documented work.
+
+It is not perfect jet but in can make:
+- Sections, Sub- and Subsections
+- Vectors, Matrices
+- Paragraphs
+- Images
+- Tables
+	- itemize and
+	2. numeric
+- and naturally of course a title
+
+
+## creating a pdf with LaTeX
+To create a pdf it uses LaTeX which makes it all flexible.
+But still there are a few things...
+
+## missing
+
+- [ ] a subtitle for more information
+- [ ] more math than mat and vec
+	- [ ] formula
+- [ ] Graphs without images
+- [ ] a solo column output file
+- [ ] a QR for a reimplement
+	- [ ] contains random seed for correction
+	- [ ] analyzable
 
 - [ ] overwork struct .tex for the rest
+
+# Try it:
+1. clone it (`git clone https://github.com/mow09/document_maker.git`)
+2. make env (`virtualenv doc_maker`) in my_path
+	- (!) `source my_path/doc_maker/bin/activate`
+3. install requirements `pip install -r requirements` in env
+4. run `python document.py` to see an example
--- document.py
+++ document.py
@@ -1,11 +1,15 @@
-#####
-#
-#   IMPORTS
-#
+"""
+Document maker.
+
+This file is made to speed up our work by making a document
+while coding in python.
+"""
 import os
 from pdflatex import PDFLaTeX
 
+
 def struct():
+    """"""
     return '\n\\usepackage[english]{babel} \n\\usepackage{microtype} \n\\usepackage{amsmath,amsfonts,amsthm} \n\\usepackage[svgnames]{xcolor} \n\\usepackage[hang, small, labelfont=bf, up, textfont=it]{caption} \n\\usepackage{booktabs} \n\\usepackage{lastpage} \n\\usepackage{graphicx}\n\\usepackage{enumitem}\n\setlist{noitemsep} \n\\usepackage{sectsty} \n \\allsectionsfont{\\usefont{OT1}{phv}{b}{n}} \n\\usepackage{geometry} \n\geometry{top=1cm, bottom=1.5cm, left=2cm, right=2cm, includehead,includefoot,}\n\setlength{\columnsep}{7mm} \n\\usepackage[T1]{fontenc} \n\\usepackage[utf8]{inputenc} \n\\usepackage{XCharter} \n\\usepackage{fancyhdr} \n\pagestyle{fancy} \n\\renewcommand{\headrulewidth}{0.0pt} \n\\renewcommand{\\footrulewidth}{0.4pt} \n \\renewcommand{\sectionmark}[1]{\markboth{#1}{}} \n \lhead{}\n\chead{\\textit{\\thetitle}} \n\\rhead{}\n\lfoot{}\n\cfoot{} \n\\fancypagestyle{firstpage}{\\fancyhf{} \\renewcommand{\\footrulewidth}{0pt}}\n\\newcommand{\\authorstyle}[1]{{\large\\usefont{OT1}{phv}{b}{n}\color{DarkBlue}#1}} \n\\newcommand{\institution}[1]{{\\footnotesize\\usefont{OT1}{phv}{m}{sl}\color{Black}#1}} \n\\usepackage{titling}\n\\newcommand{\HorRule}{\color{black}\\rule{\linewidth}{1pt}} \n\pretitle{	\\vspace{-30pt} 	\HorRule\\vspace{10pt} \\fontsize{32}{36}\\usefont{OT1}{phv}{b}{n}\selectfont \color{DarkBlue}}\n\posttitle{\par\\vskip 10pt} \n\preauthor{} \n\postauthor{ 	\\vspace{10pt} 	\par\HorRule \\vspace{0pt}}\n\\usepackage{lettrine} \n\\usepackage{fix-cm}	\n\\newcommand{\initial}[1]{ 	\lettrine[lines=3,findent=4pt,nindent=0pt]{		\color{DarkGoldenrod}		{#1}}{}}\n\\usepackage{xstring} \n\\newcommand{\lettrineabstract}[1]{	\StrLeft{#1}{1}[\\irstletter] 	\initial{\\irstletter}\\textbf{\StrGobbleLeft{#1}{llsectionsfont1}} }\n\\usepackage[backend=bibtex,style=authoryear,natbib=true]{biblatex} \n\\addbibresource{example.bib} \n\\usepackage[autostyle=true]{csquotes}'
 
 #####
@@ -13,10 +17,12 @@ def struct():
 #   CLASSES
 #
 
+
 class Title:
     def __init__(self, text):
         self.text = text
-        #self.subt = subt
+        # self.subt = subt
+
     def get_tex(self):
         return '\documentclass[10pt, a4paper, twocolumn]{article}\n\input{structure.tex}\n\\usepackage{lipsum}\n\\title{%s}\n\date{}\n\\begin{document}\n\maketitle \\thispagestyle{firstpage}' % (self.text)
 
@@ -25,19 +31,19 @@ class Title:
 #
 #   TODO:
 #   - split Titel stuff for sub etc...
-#.  - make it fancy
+# .  - make it fancy
 #   - check for one column text
 #   - prepare for math exersice
 #
 
 
-#class SubTitle:
+# class SubTitle:
 #    def __init__(self, text):
 #        self.text = text
 #    def get_tex(self):
 #        return '\\author{\\authorstyle{%s}}\n ' % (self.text)
 
-#class SubSubTitle:
+# class SubSubTitle:
 #    def __init__(self, text):
 #        self.text = text
 #    def get_tex(self):
@@ -46,60 +52,72 @@ class Title:
 class Section:
     def __init__(self, text):
         self.text = text
+
     def get_tex(self):
         return '\n\section{%s} ' % (self.text)
+
 
 class SubSection:
     def __init__(self, text):
         self.text = text
+
     def get_tex(self):
         return '\n\subsection{%s} ' % (self.text)
+
 
 class SubSubSection:
     def __init__(self, text):
         self.text = text
+
     def get_tex(self):
         return '\n\subsubsection{%s} ' % (self.text)
+
 
 class Table:
     def __init__(self, style, item_list):
         self.style = style
         self.item_list = item_list
+
     def get_tex(self):
         s_b = '\\begin{%s}\n' % (self.style)
         s_e = '\end{%s}\n' % (self.style)
         s = s_b
 
-        for i in range(0,len(self.item_list)):
-            s = s + '\t\item %s\n' % (self.item_list[i]) +' '
-        return(s + s_e) # + '\ '.strip()
+        for i in range(0, len(self.item_list)):
+            s = s + '\t\item %s\n' % (self.item_list[i]) + ' '
+        return(s + s_e)  # + '\ '.strip()
+
 
 class Image:
     def __init__(self, path, title):
         self.path = path
         self.title = title
+
     def get_tex(self):
         return '\n\\begin{figure}\includegraphics[width=\linewidth]{%s} \caption{%s} \end{figure}\n' % (self.path, self.title)
+
 
 class Paragraph:
     def __init__(self, text):
         self.text = text
+
     def get_tex(self):
         return '\n%s' % (self.text)
 
+
 class Matrix:
-    def __init__(self,M,m_n):
+    def __init__(self, M, m_n):
         self.M = M
         self.m_n = m_n
 
     def build_matrix(self):
         m_mid = ''
         for m in range(len(self.m_n)):
-            #print(m)
-            #%s & %s & %s\n %s & %s & %s \n
+            # print(m)
+            # %s & %s & %s\n %s & %s & %s \n
             for n in range(len(self.m_n[m])):
-                #print('n',n)
-                #print(m_n[m][n])
+                # print('n',n)
+                # print(m_n[m][n])
                 m_mid = m_mid + str(self.m_n[m][n])
                 if (n < len(self.m_n[m])-1):
                     m_mid = m_mid + ' & '
@@ -116,6 +134,7 @@ class Matrix:
         m_end = '\end{bmatrix}'
         return '\n$ %s = %s %s %s $ ' % (self.M, m_begin, self.build_matrix(), m_end)
 
+
 class Vector:
     def __init__(self, v, ve_list):
         self.ve_list = ve_list
@@ -129,6 +148,7 @@ class Vector:
             else:
                 v_mid = v_mid + str(self.ve_list[m]) + ' \n '
         return v_mid
+
     def get_tex(self,):
         v_beg = '\n\\left(\n \\begin{array}{c}\n'
         v_end = '\end{array}\n \\right)\n'
@@ -140,6 +160,8 @@ class Vector:
 #
 #   MAIN Document
 #
+
+
 class Document:
     def __init__(self):
         self.blocks = []
@@ -148,12 +170,11 @@ class Document:
         t = Title(text)
         self.blocks.append(t)
 
-
-    #def add_subtitle(self, text):
+    # def add_subtitle(self, text):
     #    t = SubTitle(text)
     #    self.blocks.append(t)
 
-    #def add_subsubtitle(self, text):
+    # def add_subsubtitle(self, text):
     #    t = SubSubTitle(text)
     #    self.blocks.append(t)
 
@@ -183,9 +204,9 @@ class Document:
         self.blocks.append(p)
 
     def add_matrix(self, M, m_n):
-        #print('here')
+        # print('here')
         m = Matrix(M, m_n)
-        #print('here')
+        # print('here')
         self.blocks.append(m)
 
     def add_vector(self, vector, m_list):
@@ -201,16 +222,16 @@ class Document:
             tex_main.write(tex)
         with open('structure.tex', 'w') as tex_struct:
             tex_struct.write(struct())
-        #time.sleep(5)
+        # time.sleep(5)
         pdfl = PDFLaTeX.from_texfile('somefile.tex')
         pdf, log, completed_process = pdfl.create_pdf(keep_pdf_file=True, keep_log_file=True)
-        #time.sleep(5)
-        os.rename('somefile.pdf','output.pdf')
-        #os.rmdir('some*')
+        # time.sleep(5)
+        os.rename('somefile.pdf', 'output.pdf')
+        # os.rmdir('some*')
         #os.system('rm some*')
         os.system('rm structure.tex')
 
-        #return tex
+        # return tex
 
     def __repr__(self):
         print("this class adds title, sub-, subsub-, ")
@@ -218,55 +239,53 @@ class Document:
 
 if __name__ == "__main__":
 
-#####
-#
-#   INPUT
-#
+    #####
+    #
+    #   INPUT
+    #
 
+    TITLE = 'This is a TITLE'
 
-    TITLE = 'THIS is a section'
+    # SUBTITLE = 'A way to describe changes on markets and there customers'
+    # SUBSUBTITLE = 'How does it work and how does it look like'
 
-    SUBTITLE = 'A way to describe changes on markets and there customers'
-    SUBSUBTITLE = 'How does it work and how does it look like'
+    SECTION = 'This is the first SECTION'
+    SECTION2 = 'This is the second SECTION'
 
-    SECTION = 'Malkov Math'
-    SECTION2 = 'BEAR and a table'
+    SUBSECTION = 'The first SUBSECTION'
+    SUBSECTION2 = "The second SUBSECTION is here"
 
-    SUBSECTION = 'The Chain Formal'
-    SUBSECTION2 = "not even a title"
-
-    SUBSUBSECTION = 'Sub sub section'
+    SUBSUBSECTION = 'The first and only SUBSUBSECTION'
 
     ITM1 = 'First  item in a list'
     ITM2 = 'Second  item in a list'
     ITM3 = 'Third  item in a list'
     ITM4 = 'fourth but not least'
 
-    items_list = [ITM1,ITM2,ITM3]
-    items_num = [ITM1,ITM2,ITM3, ITM4]
+    items_list = [ITM1, ITM2, ITM3]
+    items_num = [ITM1, ITM2, ITM3, ITM4]
 
     LAYER1 = 'itemize'
     LAYER2 = 'enumerate'
 
-    #PAR = ['\lipsum[2-4]']*10
+    # PAR = ['\lipsum[2-4]']*10
 
-    #IMAGE
-    IMAGE_TITLE = 'BEAR, man...'
-    IMAGE_PATH = 'bear.jpg'
-
+    # IMAGE
+    IMAGE_TITLE = 'This is a very smal example image.'
+    IMAGE_PATH = 'example.png'
 
     PAR = ['\n\lipsum[1-2]\n',
-     '\n\lipsum[2-3]\n',
-     '\n\lipsum[4-5]\n',
-     '\n\lipsum[5-7]\n',
-     '\n\lipsum[8-9]\n',
-     '\n\lipsum[10-12]\n',
-     '\n\lipsum[13-17]\n',
-     '\n\lipsum[18-19]\n',
-     '\n\lipsum[20-21]\n',
-     '\n\lipsum[22-23]\n']
+           '\n\lipsum[2-3]\n',
+           '\n\lipsum[4-5]\n',
+           '\n\lipsum[5-6]\n',
+           '\n\lipsum[8-9]\n',
+           '\n\lipsum[10-11]\n',
+           '\n\lipsum[13-15]\n',
+           '\n\lipsum[18-19]\n',
+           '\n\lipsum[20-21]\n',
+           '\n\lipsum[22-23]\n']
 
-    m_n = [[1,2,3,2],[4,5,6,2],[7,8,9,2]]
+    m_n = [[1, 2, 3, 2], [4, 5, 6, 2], [7, 8, 9, 2]]
     M = 'P'
 
 #####
@@ -278,13 +297,15 @@ if __name__ == "__main__":
 
     paper.add_title(TITLE)
 
-    #paper.add_subtitle(SUBTITLE)
+    # paper.add_subtitle(SUBTITLE)
 
-    #paper.add_subsubtitle(SUBSUBTITLE)
+    # paper.add_subsubtitle(SUBSUBTITLE)
 
     paper.add_section(SECTION)
 
     paper.add_paragraph(PAR[1])
+
+    paper.add_matrix(M, m_n)
 
     paper.add_subsection(SUBSECTION)
 
@@ -310,11 +331,9 @@ if __name__ == "__main__":
 
     paper.add_paragraph(PAR[7])
 
-    #paper.add_image(IMAGE_PATH,IMAGE_TITLE)
+    paper.add_image(IMAGE_PATH, IMAGE_TITLE)
 
     paper.add_paragraph(PAR[8])
-
-    paper.add_matrix(M, m_n)
 
     paper.add_paragraph(PAR[9])
 
